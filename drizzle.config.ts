5038ab66a4dc47701fc2c96da472ae8ce96e245a
vrishbhanusingh <vrishbhanu28@gmail.com>
2024-05-24 20:06:57 +0530

    initial

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["vrish-nextjs-t2-tut_*"],
+} satisfies Config;
